51a75e1672573635b233f69f413743369c1eeac5
Esteban Padilla <ep@estebanpadilla.com>
2017-01-01 11:36:52 -0600

    Save

--- js/canvas.js
+++ js/canvas.js
@@ -25,30 +25,24 @@ textBaseline
 */
 
 window.addEventListener('load', init, false);
-
-var canvas = undefined;
-var context = undefined;
-
-const canvasXPos = 10;
-const canvasYPos = 60;
-const canvasWidth = 420
-const canvasHeight = 120;
-
-var box = undefined;
-var ball = undefined;
-var triangle = undefined;
-var boxState = undefined;
-
 function init() {
 
-    console.log('load');
-    canvas = createCanvas(canvasXPos, canvasYPos, canvasWidth, canvasHeight);
-    context = canvas.getContext('2d');
-
+    var canvas = undefined;
+    var context = undefined;
+    var box = undefined;
+    var ball = undefined;
+    var triangle = undefined;
+    var boxState = undefined;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
     var x = 20;
     var y = 20;
-    var width = 80;
-    var height = 80;
+
+    canvas = createCanvas(width, height);
+    context = canvas.getContext('2d');
+
+    width = 80;
+    height = 80;
 
     box = new Box(x, y, width, height, 'red');
     x += 100;
@@ -74,9 +68,9 @@ function init() {
 
         context.fillStyle = boxState.color;//Yellow color
 
-        context.restore();//Restore to last state save, green
-        context.restore();//Restore to last state save, blue
-        context.restore();//Restore to last state save, red
+        //context.restore();//Restore to last state save, green
+        //context.restore();//Restore to last state save, blue
+        //context.restore();//Restore to last state save, red
         boxState.draw(context);
 
         requestAnimationFrame(update);
@@ -85,14 +79,14 @@ function init() {
     update();
 }
 
-function createCanvas(x, y, width, height) {
-    var _canvas = document.createElement('canvas');
-    _canvas.style.position = 'absolute';
-    _canvas.width = width;
-    _canvas.height = height;
-    _canvas.style.left = '' + x + 'px';
-    _canvas.style.top = '' + y + 'px';
-    _canvas.style.background = '#f6e6ca';
-    document.body.appendChild(_canvas);
-    return _canvas;
+function createCanvas(width, height) {
+    var canvas = document.createElement('canvas');
+    canvas.style.position = 'absolute';
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.left = '0px';
+    canvas.style.top = '0px';
+    canvas.style.background = '#f6e6ca';
+    document.body.appendChild(canvas);
+    return canvas;
 }
